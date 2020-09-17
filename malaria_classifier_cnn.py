d2f1ec52f5ae2d206634eb5b18e996d69a765f94
Zeeshan Patel <zeeshanp@berkeley.edu>
2020-09-16 21:19:45 -0700

    updated iou code

--- malaria_classifier_cnn.py
+++ malaria_classifier_cnn.py
@@ -1,123 +1,182 @@
-"""
-Code to classify infected and uninfected blood cells with malaria
-"""
-
-
-import numpy as np
-np.random.seed(1000)
-
-import cv2
+import tensorflow as tf
 import os
-from PIL import Image
-import keras
-from sklearn import model_selection
-from keras.utils import to_categorical
+import random
+import numpy as np
+ 
+from tqdm import tqdm 
+
+from skimage.io import imread, imshow
+from skimage.transform import resize
 import matplotlib.pyplot as plt
 
+seed = 42
+np.random.seed = seed
 
-os.environ['KERAS_BACKEND'] = 'tensorflow'
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
 
-image_directory = 'images/malarial_cell_images/'
-SIZE = 64
-dataset = []
-label = []
+TRAIN_PATH = 'stage1_train/'
+TEST_PATH = 'stage1_test/'
 
-parasitized_images = os.listdir(image_directory + 'Parasitized/')
+train_ids = next(os.walk(TRAIN_PATH))[1]
+test_ids = next(os.walk(TEST_PATH))[1]
 
-for i, image_name in enumerate(parasitized_images):
-    if (image_name.split('.')[1] == 'png'):
-        image = cv2.imread(image_directory+'Parasitized/' + image_name)
-        image = Image.fromarray(image, 'RGB')
-        image = image.resize((SIZE, SIZE))
-        dataset.append(np.array(image))
-        label.append(0)
+X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
 
-uninfected_images = os.listdir(image_directory + 'Uninfected/')
-for i, image_name in enumerate(uninfected_images):
-    if (image_name.split('.')[1] == 'png'):
-        image = cv2.imread(image_directory + 'Uninfected/' + image_name)
-        image = Image.fromarray(image, 'RGB')
-        image = image.resize((SIZE, SIZE))
-        dataset.append(np.array(image))
-        label.append(1)
+print('Resizing training images and masks')
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):   
+    path = TRAIN_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]  
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_train[n] = img  #Fill empty X_train with values from img
+    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    for mask_file in next(os.walk(path + '/masks/'))[2]:
+        mask_ = imread(path + '/masks/' + mask_file)
+        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',  
+                                      preserve_range=True), axis=-1)
+        mask = np.maximum(mask, mask_)  
+            
+    Y_train[n] = mask   
 
-        #########################################################################
+# test images
+X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+sizes_test = []
+print('Resizing test images') 
+for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+    path = TEST_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    sizes_test.append([img.shape[0], img.shape[1]])
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_test[n] = img
 
-# Convolutional Neural Network
+print('Done!')
 
-
-INPUT_SHAPE = (SIZE, SIZE, 3)
-inp = keras.layers.Input(shape=INPUT_SHAPE)
-
-conv1 = keras.layers.Conv2D(32, kernel_size=(3,3),
-                            activation='relu', padding='same')(inp)
-
-pool1 = keras.layers.MaxPool2D(pool_size=(2,2))(conv1)
-norm1 = keras.layers.BatchNormalization(axis=-1)(pool1)
-drop1 = keras.layers.Dropout(rate=0.2)(norm1)
-
-conv2 = keras.layers.Conv2D(32, kernel_size=(3,3),
-                            activation='relu', padding='same')(drop1)
-
-pool2 = keras.layers.MaxPool2D(pool_size=(2,2))(conv2)
-norm2 = keras.layers.BatchNormalization(axis=-1)(pool2)
-drop2 = keras.layers.Dropout(rate=0.2)(norm2)
-
-flat = keras.layers.Flatten()(drop2)  # Flatten matrix to prepare for dense layer
-
-hidden1 = keras.layers.Dense(512, activation='relu')(flat)
-norm3 = keras.layers.BatchNormalization(axis=-1)(hidden1)
-drop3 = keras.layers.Dropout(rate=0.2)(norm3)
-
-hidden2 = keras.layers.Dense(256, activation='relu')(drop3)
-norm4 = keras.layers.BatchNormalization(axis=-1)(hidden2)
-drop4 = keras.layers.Dropout(rate=0.2)(norm4)
-
-out = keras.layers.Dense(2, activation='sigmoid')(drop4)
-
-model = keras.Model(inputs=inp, outputs=out)
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-print(model.summary())
-
-# Can also use the model.add() methodology to build neural net, but
-# this method provides the user with an exact understanding of every layer
-
-#########################################################################################
-
-# Splitting Data Set
-
-x_train, x_test, y_train, y_test = model_selection.train_test_split(dataset,
-                                             to_categorical(np.array(label)),
-                                            test_size= 0.20, random_state=0)
-
-history = model.fit(np.array(x_train), y_train, batch_size=64, verbose=1, epochs=25, validation_split=0.1,
-                    shuffle = False)
-print("Test Accuracy: {:.2f}%".format(model.evaluate(np.array(x_test), np.array(y_test))[1]*100))
-
-f, (ax1, ax2) = plt.subplots(1,2, figsize=(12,4))
-t = f.suptitle('CNN Performance', fontsize=12)
-f.subplots_adjust(top=0.85, wspace=0.3)
-
-max_epoch = len(history.history['accuracy']) + 1
-epoch_list = list(range(1, max_epoch))
-ax1.plot(epoch_list, history.history['accuracy'], label='Train Accuracy')
-ax1.plot(epoch_list, history.history['val_accuracy'], label='Validation Accuracy')
-ax1.set_xticks(np.arange(1, max_epoch, 5))
-ax1.set_ylabel('Accuracy Value')
-ax1.set_xlabel('Epoch')
-ax1.set_title('Accuracy')
-legend_1 = ax1.legend(loc='best')
-
-ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
-ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
-ax2.set_xticks(np.arange(1, max_epoch, 5))
-ax2.set_ylabel('Loss Value')
-ax2.set_xlabel('Epoch')
-ax2.set_title('Loss')
-legend_2 = ax2.legend(loc='best')
+image_x = random.randint(0, len(train_ids))
+imshow(X_train[image_x])
+plt.show()
+imshow(np.squeeze(Y_train[image_x]))
+plt.show()
 
 
-model.save('malaria_cnn.h5')
+
+
+#Build the model
+inputs = tf.keras.layers.Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
+
+#Contraction path
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+c1 = tf.keras.layers.Dropout(0.1)(c1)
+c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+c2 = tf.keras.layers.Dropout(0.1)(c2)
+c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+ 
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+c3 = tf.keras.layers.Dropout(0.2)(c3)
+c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+ 
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+c4 = tf.keras.layers.Dropout(0.2)(c4)
+c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+ 
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+c5 = tf.keras.layers.Dropout(0.3)(c5)
+c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+#Expansive path 
+u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+u6 = tf.keras.layers.concatenate([u6, c4])
+c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+c6 = tf.keras.layers.Dropout(0.2)(c6)
+c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+ 
+u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+u7 = tf.keras.layers.concatenate([u7, c3])
+c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+c7 = tf.keras.layers.Dropout(0.2)(c7)
+c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+ 
+u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+u8 = tf.keras.layers.concatenate([u8, c2])
+c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+c8 = tf.keras.layers.Dropout(0.1)(c8)
+c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+ 
+u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+c9 = tf.keras.layers.Dropout(0.1)(c9)
+c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+ 
+outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+ 
+model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+
+################################
+
+#Modelcheckpoint
+checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_nuclei.h5', verbose=1, save_best_only=True)
+
+callbacks = [
+        tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+        tf.keras.callbacks.TensorBoard(log_dir='logs')]
+
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=25, callbacks=callbacks)
+
+####################################
+
+# Viewing Reuslts of U-Net
+idx = random.randint(0, len(X_train))
+
+
+preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+preds_test = model.predict(X_test, verbose=1)
+
+ 
+preds_train_t = (preds_train > 0.5).astype(np.uint8)
+preds_val_t = (preds_val > 0.5).astype(np.uint8)
+preds_test_t = (preds_test > 0.5).astype(np.uint8)
+
+
+# Perform a sanity check on some random training samples
+ix = random.randint(0, len(preds_train_t))
+imshow(X_train[ix])
+plt.show()
+imshow(np.squeeze(Y_train[ix]))
+plt.show()
+imshow(np.squeeze(preds_train_t[ix]))
+plt.show()
+
+# Perform a sanity check on some random validation samples
+ix = random.randint(0, len(preds_val_t))
+imshow(X_train[int(X_train.shape[0]*0.9):][ix])
+plt.show()
+imshow(np.squeeze(Y_train[int(Y_train.shape[0]*0.9):][ix]))
+plt.show()
+imshow(np.squeeze(preds_val_t[ix]))
+plt.show()
+
+################################
+
+#IoU (Intersection over Union, Jaccard Index) Calculation
+
+axes = (0,1)
+intersection = np.sum(np.logical_and(mask_f_bw, mask_o), axis=axes)
+union = np.sum(np.logical_or(mask_f_bw, mask_o), axis=axes)
+smooth = 0.001
+iou = (intersection + smooth) / (union + smooth)
+iou = np.mean(iou)
+print(iou)
+
+################################
